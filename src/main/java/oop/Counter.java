6a1afcea392924ef08e33fc17d6fcd43ef5a5248
robert mcateer <robertmcateer2000@hotmail.co.uk>
2020-11-04 10:23:46 +0000

    first commit

--- src/main/java/oop/Counter.java
+++ src/main/java/oop/Counter.java
@@ -39,12 +39,26 @@ public class Counter {
     // the value of this counter to 0.
     // Test by calling from App start()
 
+    public void reset(){
+        this.count = 0;
+        System.out.println("counter reset"+count);
+    }
+
 
     //TODO
     // Implement an undo() method that will reduce the count by one.
     // Do not allow the count to become negative.
     // Print the value of count from the method after the decrement.
     // Call this undo() method from App start().
+
+    public void undo(){
+        if(this.count > 0 ){
+            this.count -= 1;
+            System.out.println("Click undo= "+this.count);
+        }else{
+            System.out.println("counter cant be a negative number");
+        }
+    }
 
 
 
